738c25b9d779f145f210f762dd85f8d5acb64fa4
Mark Olson <mdo@ieee.org>
2019-11-03 09:12:42 -0800

    arduino test code and start of real code

---
+++ RBG_arduino/RBG_SciFi/GradCap.h
@@ -0,0 +1,67 @@
+
+// Mark Olson 2019-11
+//
+// Major kudos to Daniel Garcia and Mark Kriegsman for the FANTASTIC FastLED library and examples!!!
+//    A sad note that Daniel Garcia, co-author of FastLED library, was on the dive boat that caught fire and has passed. 
+//    Here is some info on the FastLED Reddit https://www.reddit.com/r/FastLED/
+//
+// I am using an Arduino Nano with a USB mini-B connector
+//   example: http://www.ebay.com/itm/Nano-V3-0-ATmega328P-5V-16M-CH340-Compatible-to-Arduino-Nano-V3-Without-Cable/201804111413?_trksid=p2141725.c100338.m3726&_trkparms=aid%3D222007%26algo%3DSIC.MBE%26ao%3D1%26asc%3D20150313114020%26meid%3Dea29973f227743f78772d7a22512af53%26pid%3D100338%26rk%3D1%26rkt%3D30%26sd%3D191602576205
+//            V3.0 ATmega328P 5V 16M CH340 Compatible to Arduino Nano V3
+//            32Kbyte Flash (program storage), 2Kbyte SRAM, 1Kbyte EEPROM
+//            http://www.mouser.com/pdfdocs/Gravitech_Arduino_Nano3_0.pdf
+//            http://www.pighixxx.com/test/pinouts/boards/nano.pdf
+//
+// Using the biggest three of two sets of LED rings: https://smile.amazon.com/gp/product/B07437X7SL/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&psc=1
+//
+// Here is a (somewhat) spec on the 2812b LEDs: https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
+// Here is a Worldsemi WS2812B document for download: http://www.world-semi.com/solution/list-4-1.html#108
+//
+// Kudos to Daniel Garcia and Mark Kriegsman for the FANTASTIC Arduino FastLED library and examples!!!
+//    https://github.com/FastLED/FastLED
+//    https://github.com/FastLED/FastLED/blob/master/examples/Fire2012WithPalette/Fire2012WithPalette.ino
+//
+// The Arduino pattern code here is pretty much done from scratch by me using the FastLED library.
+//    I also tried a few items from Mark Kriegsman's classic DemoReel100.ino https://gist.github.com/kriegsman/062e10f7f07ba8518af6
+//
+// These LEDs use power that adds up. Can use this to estimate the power
+//   http://fastled.io/docs/3.1/group___power.html
+//     calculate_max_brightness_for_power_vmA(lots of parameters)
+//   https://github.com/FastLED/FastLED/blob/master/power_mgt.cpp
+//     static const uint8_t gRed_mW   = 16 * 5; // 16mA @ 5v = 80mW
+//     static const uint8_t gGreen_mW = 11 * 5; // 11mA @ 5v = 55mW
+//     static const uint8_t gBlue_mW  = 15 * 5; // 15mA @ 5v = 75mW
+//     static const uint8_t gDark_mW  =  1 * 5; //  1mA @ 5v =  5mW
+//   about 42 milliamps per LED at max brightness WHITE
+//
+// Calculating wire size (Google for instance wire gauge ampacity 5v dc):
+//    https://electronics.stackexchange.com/questions/148648/minimum-wire-gauge-for-5-volt-5-amp-system
+//
+// connections:
+//    Data Pin 3 is used for serial communications with the LEDs
+// 
+// Recommendations -  ;^)
+//    Before connecting the WS2812 to a power source, connect a big capacitor from power to ground.
+//      A cap between 100microF and 1000microF should be good. Try to place this cap as close to your WS2812 as possible.
+//      Electrolytic Decoupling Capacitors 
+//    Placing a small-ish resistor between your Arduino's data output and the WS2812's data input will help protect the data pin. A resistor between 220 and 470 O should do nicely. Try to place the resistor as close to the WS2812 as possible.
+//    Keep Wires Short!
+//    https://github.com/FastLED/FastLED/wiki/Wiring-leds
+//
+
+#define BRIGHTMAX 40 // set to 250 for final version
+
+// I am using the biggest three of 6 rings from a 93-LED disk - 72 LEDs
+#define NUM_DISKS 1 // definitely not enough room for multiple disks in one Arduino
+#define NUM_LEDS_PER_DISK 72
+#define NUM_RINGS_PER_DISK 3
+#define NUM_SHADOWS 1  // number of shadow disks
+
+// LED count - number of LEDs in each ring in order of serial access
+const uint8_t  leds_per_ring[NUM_RINGS_PER_DISK]  = { 32, 24, 16 };
+const uint8_t  leds_per_ringqrtr[NUM_RINGS_PER_DISK]  = { 8, 6, 4 };
+const uint8_t  start_per_ring[NUM_RINGS_PER_DISK] = {  0, 32, 56 };
+
+#define LED_TYPE     WS2812
+#define COLOR_ORDER  GRB
+#define FRAMES_PER_SECOND  120
---
+++ RBG_arduino/RBG_SciFi/RBG_SciFi.ino
@@ -0,0 +1,160 @@
+// Author: Mark Olson 2019-11-03
+//
+// Rubber Band Gun - https://github.com/Mark-MDO47/RubberBandGun
+// RBG - A high-tech imagining of the rubber band gun
+//
+// Major kudos to Daniel Garcia and Mark Kriegsman for the FANTASTIC FastLED library and examples!!!
+//    A sad note that Daniel Garcia, co-author of FastLED library, was on the dive boat that caught fire and has passed. 
+//    Here is some info on the FastLED Reddit https://www.reddit.com/r/FastLED/
+//
+// I am using an Arduino Nano with a USB mini-B connector
+//   example: http://www.ebay.com/itm/Nano-V3-0-ATmega328P-5V-16M-CH340-Compatible-to-Arduino-Nano-V3-Without-Cable/201804111413?_trksid=p2141725.c100338.m3726&_trkparms=aid%3D222007%26algo%3DSIC.MBE%26ao%3D1%26asc%3D20150313114020%26meid%3Dea29973f227743f78772d7a22512af53%26pid%3D100338%26rk%3D1%26rkt%3D30%26sd%3D191602576205
+//            V3.0 ATmega328P 5V 16M CH340 Compatible to Arduino Nano V3
+//            32Kbyte Flash (program storage), 2Kbyte SRAM, 1Kbyte EEPROM
+//            http://www.mouser.com/pdfdocs/Gravitech_Arduino_Nano3_0.pdf
+//            http://www.pighixxx.com/test/pinouts/boards/nano.pdf
+//
+// Using the biggest three of two sets of LED rings: https://smile.amazon.com/gp/product/B07437X7SL/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&psc=1
+//
+// Also using a YX5200 sound player
+//   The YX5200 uses FAT32-formatted TF (or SD ?) card up to 32 GByte
+//   These look like a helpful sites for the YX5200:
+//   https://wiki.dfrobot.com/DFPlayer_Mini_SKU_DFR0299
+//   https://github.com/DFRobot/DFRobotDFPlayerMini
+// The YX5200 interface code is based on the "FullFunction.ino" DFPlayer.
+//   this uses the GNU Lesser General Public License.
+//
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"                  // to talk to myDFPlayer without using up debug serial port
+#include <EEPROM.h>                          // to store configuration info
+
+#include "FastLED.h"                         // to manipulate WS2812b 5050 RGB LED Rings
+#include "DFRobotDFPlayerMini.h"             // to communicate with the YX5200 audio player
+
+#define SERIALDEBUG 1                        // serial debugging
+// #define DFPRINTDETAIL (1&SERIALDEBUG)     // if need detailed status from myDFPlayer
+#define DFPRINTDETAIL 0                      // will not print detailed status from myDFPlayer
+void DFprintDetail(uint8_t type, int value); // definition of call doesn't hurt
+
+#define DPIN_FASTLED      3  // talk to FASTLED library
+#define DPIN_BTN_TRIGGER  4
+#define DPIN_BTN_YELLOW   5
+#define DPIN_BTN_GREEN    6
+#define DPIN_BTN_BLACK    7
+#define DPIN_SWSRL_RX    10
+#define DPIN_SWSRL_TX    11
+#define DPIN_AUDIO_BUSY  12
+#define DPIN_SOLENOID    13  // often has internal LED and resistor soldered to board, can make INPUT not work
+
+
+SoftwareSerial mySoftwareSerial(DPIN_SWSRL_RX, DPIN_SWSRL_TX); // to talk to YX5200 audio player
+DFRobotDFPlayerMini myDFPlayer;                                // to talk to YX5200 audio player
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+#if SERIALDEBUG
+  Serial.begin(115200);         // this is for general debug
+#endif // SERIALDEBUG
+  mySoftwareSerial.begin(9600); // this is control to DFPlayer audio player
+
+  Serial.println();
+  Serial.println(F("FOOF SciFi RBG init..."));
+
+  pinMode(DPIN_BTN_TRIGGER, INPUT);
+  pinMode(DPIN_BTN_YELLOW,  INPUT);
+  pinMode(DPIN_BTN_GREEN,   INPUT);
+  pinMode(DPIN_BTN_BLACK,   INPUT);
+  pinMode(DPIN_AUDIO_BUSY,  INPUT);
+  pinMode(DPIN_SOLENOID,   OUTPUT);
+} // end setup()
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  static unsigned long timer = millis();
+
+}  // end loop()
+
+void setupDFPlayer() {
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  // Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin DFPlayer:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+
+}
+
+#if DFPRINTDETAIL
+void DFprintDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+} // end DFprintDetail()
+#else  // no DFPRINTDETAIL
+void DFprintDetail(uint8_t type, int value) {} // just return
+#endif // DFPRINTDETAIL
