9e8d6cc2cabd136d538d2a5c9652d10627342a68
Mark Olson <mdo@ieee.org>
2019-11-09 20:02:48 -0800

    improved makeStateTable

--- RBG_arduino/makeStateTable.py
+++ RBG_arduino/makeStateTable.py
@@ -15,6 +15,16 @@ SYMBTABLE = {}
 SYMBTABLEROW = { "blockStart": -1, "blockEnd": -1 }
 STATETABLE = {}
 STATETABLEROW = { "blkFlags": 0, "soundAfterInput": 0, "lights": 0, "inputRBG": 0, "storeVal": 0, "storeAddr": 0, "gotoOnInput": 0, "gotoWithoutInput": 0 }
+FOUNDINCOLUMN = {
+   "index": [],
+   "soundAfterInput": [],
+   "lights": [],
+   "inputRBG": [],
+   "storeVal": [],
+   "storeAddr": [],
+   "gotoOnInput": [],
+   "gotoWithoutInput": []
+}
 
 # INPUT:
 COLTOINDEX = {"index": -1, "soundAfterInput": -1, "lights": -1, "inputRBG": -1, "storeVal": -1, "storeAddr": -1, "gotoOnInput": -1, "gotoWithoutInput": -1 }
@@ -27,6 +37,35 @@ COLTOINDEX = {"index": -1, "soundAfterInput": -1, "lights": -1, "inputRBG": -1, 
   # uint8_t storeAddr;        // address to store; includes mask for mFUNC, mVAL, eeSoundSave|mFUNC: idx= 1 WindUp, 2 Shoot, 4 Open, 7 Load
   # uint8_t gotoOnInput;      // index within table to go with matching input    
   # uint8_t gotoWithoutInput; // index within table to go without waiting for input
+
+translateToMasks = {
+   "blkFlags": {},
+   "soundAfterInput": {},
+   "lights": {},
+   "inputRBG": {
+      "trigPlus": "mTRIG|mBANY",
+      "trig00": "mTRIG|mBNONE",
+      "trig01": "mTRIG|mB01",
+      "trig02": "mTRIG|mB02",
+      "trig03": "mTRIG|mB03",
+      "trig04": "mTRIG|mB04",
+      "trig05": "mTRIG|mB05",
+      "trig06": "mTRIG|mB06",
+      "trig07": "mTRIG|mB07",
+      "open": "mOPEN",
+      "lock": "mLOCK",
+      "": "mNONE",
+      "trigOnly": "mTRIG|mBNONE",
+      "trigYellow": "mTRIG|mB01",
+      "trigGreen": "mTRIG|mB02",
+      "trigBlack": "mTRIG|mB03",
+      "trigAll": "mTRIG|mB07",
+   },
+   "storeVal": {},
+   "storeAddr": {},
+   "gotoOnInput": {},
+   "gotoWithoutInput": {}
+}
 
 
 def MarkEndBlock(currSymb, currStateTableIdx):
@@ -54,13 +93,35 @@ def MakeNewBlock(currSymb: object, currStateTableIdx: object, debugString: objec
       print("DEBUG NEW %s BEFORE: currStateTableIdx %d %s" % (debugString, currStateTableIdx, currSymb))
    print("  DEBUG SYMBTABLE %s" % SYMBTABLE)
    currStateTableIdx += 1
-   STATETABLE[currStateTableIdx] = copy.deepcopy(STATETABLEROW)
    SYMBTABLE[currSymb] = copy.deepcopy(SYMBTABLEROW)
    SYMBTABLE[currSymb]["blockStart"] = currStateTableIdx
 
    print("DEBUG NEW %s  AFTER: currStateTableIdx %d SYMBTABLE[%s] %s" % (debugString, currStateTableIdx, currSymb, SYMBTABLE[currSymb]))
    print("  DEBUG SYMBTABLE %s" % SYMBTABLE)
    return currStateTableIdx, currSymb
+
+
+# COLTOINDEX = {"index": "", "soundAfterInput", "lights", "input", "storeVal", "storeAddr", "gotoOnInput", "gotoWithoutInput": "" }
+# STATETABLEROW = { "blkFlags": 0, "soundAfterInput": 0, "lights": 0, "inputRBG": 0, "storeVal": 0, "storeAddr": 0, "gotoOnInput": 0, "gotoWithoutInput": 0 }
+def FillStateTablePass1(row, stateIdx):
+   global FOUNDINCOLUMN
+   global STATETABLE
+
+   STATETABLE[stateIdx] = copy.deepcopy(STATETABLEROW)
+   for key in COLTOINDEX.keys():
+      rowText = str(row[key]).strip()
+      if "nan" == rowText:
+         rowText = "mNONE"
+      if rowText not in FOUNDINCOLUMN[key]:
+         FOUNDINCOLUMN[key].append(rowText)
+      if key in translateToMasks.keys():
+         if rowText in translateToMasks[key]:
+            rowText = translateToMasks[key][rowText]
+         else:
+            pass
+      if key in STATETABLEROW.keys():
+         STATETABLE[stateIdx][key] = rowText
+
 
 def MakeStateTable():
    global SYMBTABLE
@@ -76,46 +137,51 @@ def MakeStateTable():
    for col in COLTOINDEX:
       COLTOINDEX[col] = dfColNames.index(col)
 
-   # read through excel file and generate state table
+   # Pass 1 - read through excel file and get symbol table
    STATETABLEIdx = -1    # this will be index into state table we are building
    SYMBTABLECurrent = "" # zero length means not processing any state decision block
+   SYMBrequired = []
    for rowNum, row in df.iterrows():
       # the first row (rowNum == 0) is the one after the column titles
       # rowIndexSymb = str(row[dfColNames["index"]]).strip
       rowIndexSymb = str(row["index"]).strip()
-      if "nan" == rowIndexSymb: # rows with nothing in "index" column are ignored except...
-         # ... they mark the end of the current state decision block
-         """
+      if "nan" == rowIndexSymb: # rows with nothing in "index" column are ignored
          err = MarkEndBlock(SYMBTABLECurrent, STATETABLEIdx)
          if 0 != len(err):
             print("DEBUG %s SYMBTABLECurrent |%s| from rows with nothing in index column" % (err, SYMBTABLECurrent))
          print("  DEBUG %s" % SYMBTABLE)
-         SYMBTABLECurrent = ""
-         """
          continue
    
       if 0 == len(SYMBTABLECurrent): # this is a new symbol, possibly the first symbol
          if STATETABLEIdx >= 0:  # mark end of previous block if there is one
             err = MarkEndBlock(SYMBTABLECurrent, STATETABLEIdx)
          STATETABLEIdx, SYMBTABLECurrent = MakeNewBlock(rowIndexSymb, STATETABLEIdx)
+         FillStateTablePass1(row, STATETABLEIdx)
       else: # we were processing a symbol before
          if rowIndexSymb == SYMBTABLECurrent: # continuing on with this state decision block
             STATETABLEIdx += 1
+            FillStateTablePass1(row, STATETABLEIdx)
          else: # new state decision block
             if STATETABLEIdx >= 0:  # mark end of previous block if there is one
                err = MarkEndBlock(SYMBTABLECurrent, STATETABLEIdx)
             STATETABLEIdx, SYMBTABLECurrent = MakeNewBlock(rowIndexSymb, STATETABLEIdx)
+            FillStateTablePass1(row, STATETABLEIdx)
 
-   print("SYMBTABLE")
+   print("Pass 1 SYMBTABLE")
    print("  %s" % SYMBTABLE)
    for symb in SYMBTABLE:
       print("  %s %s" % (symb, SYMBTABLE[symb]))
 
-   print("STATETABLE")
+   print("Pass 1 STATETABLE")
+   print("  %s" % STATETABLE)
    for idx in STATETABLE:
-      print("  %d %s" % (idx, STATETABLE[idx]))
+      print("  %s %s" % (idx, str(STATETABLE[idx])))
 
-   # COLTOINDEX = {"index": "", "soundAfterInput", "lights", "input", "storeVal", "storeAddr", "gotoOnInput", "gotoWithoutInput": "" }
+   print("Pass 1 FOUNDINCOLUMN")
+   print("  %s" % FOUNDINCOLUMN)
+   for key in FOUNDINCOLUMN.keys():
+      print("  %s %s" % (key, FOUNDINCOLUMN[key]))
+
 
 if __name__ == "__main__": 
    MakeStateTable()
