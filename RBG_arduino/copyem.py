eff75ebc98249128d96552c67e1e135717ee855a
Mark Olson <mdo@ieee.org>
2020-09-14 21:53:51 -0700

    use argparse for copyem.py

--- RBG_arduino/copyem.py
+++ RBG_arduino/copyem.py
@@ -6,64 +6,62 @@
 #
 import os
 import sys
+import argparse
 
 theFnames = []
 maxNum = 127  # beyond that is not currently used
-fnameSilence = "0069__mdo47__silence.wav"  # to fill in gaps
-theFrom = "D:/svnNew/SoundsSciFi/SoundsRecordedMDO/secondCut"
-theSD = "H:"
+# fnameSilence = "0069__mdo47__silence.wav"  # to fill in gaps
+# theFrom = "D:/svnNew/SoundsSciFi/SoundsRecordedMDO/secondCut"
+# theSD = "H:"
 
-for fname in os.listdir(theFrom):
-    if (fname.lower().endswith(".wav")) and (maxNum >= int(fname[:4])):
-        theFnames.append(fname.lower())
-theFnames.sort()
-theMaxNum = int(theFnames[-1][:4])
-
-"""
-for fname in theFnames:
-   print("%s" % fname)
-"""
-
-# first the root directory
-count = 1 # we do not want number 0 to be copied
-for fname in theFnames:
-    fcount = int(fname[:4])
-    if fcount < count:
-        continue # we do not want number 0 to be copied
-    for idx in range(count, fcount):
-        print("cp %s/%s %s/%03d.wav" % (theFrom, fnameSilence, theSD, idx))
-    print("cp %s/%s %s/%03d.wav" % (theFrom, fname, theSD, fcount))
-    count = fcount + 1
-
-"""
-# now the folders 01, 02 etc. Note that folder 00 is not used!!!
-print(" ")
-numFolders = 1 + theMaxNum // 10
-for dir in range(1, 1 + numFolders):
-    print("mkdir %s/%02d" % (theSD, dir))
-    count = minFnum = 1 + 10 * (dir - 1)
-    maxFnum = minFnum + 8
+def doCopyem(theFrom = None, theSD = None, fnameSilence = None, copyCmd = None):
+    for fname in os.listdir(theFrom):
+        if (fname.lower().endswith(".wav")) and (maxNum >= int(fname[:4])):
+            theFnames.append(fname.lower())
+    theFnames.sort()
+    theMaxNum = int(theFnames[-1][:4])
+    
+    """
+    for fname in theFnames:
+       print("%s" % fname)
+    """
+    
+    # first the root directory
+    count = 1 # we do not want number 0 to be copied
     for fname in theFnames:
         fcount = int(fname[:4])
-        if fcount < minFnum:
-            continue
-        for idx in range(count, min(fcount, maxFnum+1)):
-            print("cp %s/%s %s/%02d/%03d.wav" % (theFrom, fnameSilence, theSD, dir, idx))
-        if fcount <= maxFnum:
-            print("cp %s/%s %s/%02d/%03d.wav" % (theFrom, fname, theSD, dir, fcount))
+        if fcount < count:
+            continue # we do not want number 0 to be copied
+        for idx in range(count, fcount):
+            print("%s %s/%s %s/%03d.wav" % (copyCmd, theFrom, fnameSilence, theSD, idx))
+        print("%s %s/%s %s/%03d.wav" % (copyCmd, theFrom, fname, theSD, fcount))
         count = fcount + 1
-        if fcount >= maxFnum:
-            break
 
-# now the MP3 folder
-print(" ")
-print("mkdir %s/MP3" % theSD)
-for fname in theFnames:
-    fcount = int(fname[:4])
-    print("cp %s/%s %s/MP3/%04d.wav" % (theFrom, fname, theSD, fcount))
-"""
+    # now the attributions
+    print(" ")
+    print("mkdir %s/ATTRIBUTIONS" % theSD)
+    print("%s  %s/Attributions.html %s/ATTRIBUTIONS" % (copyCmd, theFrom, theSD))
 
-# now the attributions
-print(" ")
-print("mkdir %s/ATTRIBUTIONS" % theSD)
-print("cp  %s/Attributions.html %s/ATTRIBUTIONS" % (theFrom, theSD))
+
+if __name__ == "__main__":
+    my_parser = argparse.ArgumentParser(prog='copyem',
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="stdout receives list of copy commands for freshly formatted SD card",
+        epilog="""Example:
+python copyem.py -d ./myAudioFiles -s H: -f soundsOfSilence.wav --linux
+""",
+        usage='%(prog)s {-d directory -s sd-disk -f fname-silence {-l linux | -w windows} }')
+    my_parser.add_argument('-d', '--directory', type=str, help='path to directory containing *.wav files', action='store', default="D:/svnNew/SoundsSciFi/SoundsRecordedMDO/secondCut")
+    my_parser.add_argument('-s', '--sd-disk', type=str, help='path to freshly formatted FAT32 SD disk (e.g. H:)', action='store', default="H:")
+    my_parser.add_argument('-f', '--fname-silence', type=str, help='filename to use as silence when break in numerical sequence', action='store', default="0069__mdo47__silence.wav")
+    my_parser.add_argument('-w', '--windows', help='Use *.bat copy commands for output file', action='store_true')
+    my_parser.add_argument('-l', '--linux', help='use *.sh cp commands for output file', action='store_true')
+    args = my_parser.parse_args()
+
+    copyCmd = None
+    if args.windows:
+        copyCmd = "copy"
+    else:
+        copyCmd = "cp"
+
+    doCopyem(theFrom=args.directory, theSD=args.sd_disk, fnameSilence=args.fname_silence, copyCmd = copyCmd)
