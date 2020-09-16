cca4ec41e565d47e4e8412dfae033f74a1a431d9
Mark Olson <mdo@ieee.org>
2020-09-15 18:49:09 -0700

    more args for copyem.py

--- RBG_arduino/copyem.py
+++ RBG_arduino/copyem.py
@@ -10,11 +10,8 @@ import argparse
 
 theFnames = []
 maxNum = 127  # beyond that is not currently used
-# fnameSilence = "0069__mdo47__silence.wav"  # to fill in gaps
-# theFrom = "D:/svnNew/SoundsSciFi/SoundsRecordedMDO/secondCut"
-# theSD = "H:"
 
-def doCopyem(theFrom = None, theSD = None, fnameSilence = None, copyCmd = None):
+def doCopyem(theFrom = None, theSD = None, fnameSilence = None, copyCmd = None, theSlash = None, noAttributeFile = False):
     for fname in os.listdir(theFrom):
         if (fname.lower().endswith(".wav")) and (maxNum >= int(fname[:4])):
             theFnames.append(fname.lower())
@@ -33,14 +30,15 @@ def doCopyem(theFrom = None, theSD = None, fnameSilence = None, copyCmd = None):
         if fcount < count:
             continue # we do not want number 0 to be copied
         for idx in range(count, fcount):
-            print("%s %s/%s %s/%03d.wav" % (copyCmd, theFrom, fnameSilence, theSD, idx))
-        print("%s %s/%s %s/%03d.wav" % (copyCmd, theFrom, fname, theSD, fcount))
+            print("%s %s%s%s %s%s%03d.wav" % (copyCmd, theFrom, theSlash, fnameSilence, theSD, theSlash, idx))
+        print("%s %s%s%s %s%s%03d.wav" % (copyCmd, theFrom, theSlash, fname, theSlash, theSD, fcount))
         count = fcount + 1
 
     # now the attributions
-    print(" ")
-    print("mkdir %s/ATTRIBUTIONS" % theSD)
-    print("%s  %s/Attributions.html %s/ATTRIBUTIONS" % (copyCmd, theFrom, theSD))
+    if False == noAttributeFile:
+        print(" ")
+        print("mkdir %s%sATTRIBUTIONS" % (theSD, theSlash))
+        print("%s  %s%sAttributions.html %s%sATTRIBUTIONS" % (copyCmd, theFrom, theSlash, theSD, theSlash))
 
 
 if __name__ == "__main__":
@@ -53,15 +51,20 @@ python copyem.py -d ./myAudioFiles -s H: -f soundsOfSilence.wav --linux
         usage='%(prog)s {-d directory -s sd-disk -f fname-silence {-l linux | -w windows} }')
     my_parser.add_argument('-d', '--directory', type=str, help='path to directory containing *.wav files', action='store', default="D:/svnNew/SoundsSciFi/SoundsRecordedMDO/secondCut")
     my_parser.add_argument('-s', '--sd-disk', type=str, help='path to freshly formatted FAT32 SD disk (e.g. H:)', action='store', default="H:")
-    my_parser.add_argument('-f', '--fname-silence', type=str, help='filename to use as silence when break in numerical sequence', action='store', default="0069__mdo47__silence.wav")
+    my_parser.add_argument('-f', '--fname-silence', type=str, help='filename within DIRECTORY to use as silence when break in numerical sequence', action='store', default="0069__mdo47__silence.wav")
     my_parser.add_argument('-w', '--windows', help='Use *.bat copy commands for output file', action='store_true')
     my_parser.add_argument('-l', '--linux', help='use *.sh cp commands for output file', action='store_true')
+    my_parser.add_argument('-n', '--no-attribute-file', help='do not include Attributions.html from DIRECTORY in an ATTRIBUTIONS directory on the sd-disk', action='store_true', default=False)
     args = my_parser.parse_args()
 
     copyCmd = None
     if args.windows:
         copyCmd = "copy"
+        theSlash = "\\"
+        args.directory = args.directory.replace("/",theSlash)
     else:
         copyCmd = "cp"
+        theSlash = "/"
+        args.directory = args.directory.replace("\\",theSlash)
 
-    doCopyem(theFrom=args.directory, theSD=args.sd_disk, fnameSilence=args.fname_silence, copyCmd = copyCmd)
+    doCopyem(theFrom=args.directory, theSD=args.sd_disk, fnameSilence=args.fname_silence, copyCmd = copyCmd, theSlash = theSlash, noAttributeFile = args.no_attribute_file)
